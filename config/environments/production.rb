60e93c54c3151241ee4d8937c683abae3113a2c3
Bryan Kahler <bkahler3@gmail.com>
2016-11-22 06:00:20 +0000

    assets fix round2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,8 +22,8 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
-  config.serve_static_assets = true
+  config.serve_static_files = true #ENV['RAILS_SERVE_STATIC_FILES'].present?
+  # config.serve_static_assets = true
   config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
   config.assets.compile = true
   # Compress JavaScripts and CSS.
