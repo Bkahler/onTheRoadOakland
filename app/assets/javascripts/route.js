c9403a148867804414debe0170cb6f7344acb06f
panicbus <elementalair@gmail.com>
2016-11-25 21:28:41 -0800

    updating proj to mapboxgl, some new marker icons

--- app/assets/javascripts/route.js
+++ app/assets/javascripts/route.js
@@ -1,19 +1,20 @@
-// all the json data for marker locations and routeline
-
-var geoJson_features = [
-	{
-	  type: "Feature",
-	  geometry: {
-	    type: "Point",
-	    coordinates: [-122.268522, 37.809220]
-	  },
-	  properties: {
-	    // title: 'Station 1',
-	    description: 'Great Wall of Oakland',
-	    'marker-color': '#548cba',
-	    'marker-size': 'large',
-	    'marker-symbol': 1,
-	    location_id: 1
-	  }
-	}
-];
+var geojson = {
+	'type': 'FeatureCollection',
+	'features': [
+		{
+			'type': 'Feature',
+			'properties': {
+				// 'message': 'The Great Wall of Oakland Project',
+				'iconSize': [128, 128],
+				'location_id': 1
+			},
+			'geometry': {
+				'type': 'Point',
+				'coordinates': [
+					-122.270213,
+					37.810661
+				]
+			}
+		}
+	]
+};
