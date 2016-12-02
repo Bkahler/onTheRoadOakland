3663f558e031668baa7ae07c54e02eaa01edea4f
Bryan Kahler <bkahler3@gmail.com>
2016-12-02 19:36:27 +0000

    removing pry .... doh!

--- app/controllers/checkins_controller.rb
+++ app/controllers/checkins_controller.rb
@@ -11,7 +11,6 @@ class CheckinsController < ApplicationController
   def create
     @checkin = Checkin.new(checkin_params)
     @location = @checkin.location
-binding.pry
     if @checkin.save
       flash[:notice] = 'Your check-in was successfully saved!'
       respond_to do |format|
