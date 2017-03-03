bd15d0ddbe4177414398e23e5b1b8136cfaaab9b
panicbus <elementalair@gmail.com>
2017-03-02 18:11:21 -0800

    updating about for 3/3 showing

--- app/assets/javascripts/all_checkins_buttons.js
+++ app/assets/javascripts/all_checkins_buttons.js
@@ -1,15 +1,15 @@
-$(window).load(function(){
+// $(window).load(function(){
 
-	// for persistent active buttons on all_checkins page
+// 	// for persistent active buttons on all_checkins page
 
-	var hoverLink = function(link){
-		$('.view-options').removeClass('current');
-		$(link).addClass('current');
-	};
+// 	var hoverLink = function(link){
+// 		$('.view-options').removeClass('current');
+// 		$(link).addClass('current');
+// 	};
 
-	$('body').on('click', '#locations_links a', function(){
-		hoverLink(this);
-	});
+// 	$('body').on('click', '#locations_links a', function(){
+// 		hoverLink(this);
+// 	});
 
 
-});
+// });
